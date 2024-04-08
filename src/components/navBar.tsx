6038829a165a074d884300baa5c312348c60acf0
theonlyshannon <sofwannuhaalfaruq@gmail.com>
2024-04-08 16:00:35 +0700

    Initiate Navbar (main)

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 const NavBar = () => {
@@ -10,22 +11,22 @@ const NavBar = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </div>
                         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a>Menu 1</a></li>
-                            <li><a>Menu 2</a></li>
-                            <li><a>Menu 3</a></li>
+                            <li><a>Login</a></li>
+                            <li><a>Register</a></li>
+                            <li><a>Dashboard</a></li>
                         </ul>
                     </div>
                     <a className="btn btn-ghost text-xl">Git-Flow</a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
-                        <li><a>Menu 1</a></li>
-                        <li><a>Menu 2</a></li>
-                        <li><a>Menu 3</a></li>
+                        <li><Link href='/login'>Login</Link></li>
+                        <li><Link href='/register'>Register</Link></li>
+                        <li><Link href='/dashboard'>Dashboard</Link></li>
                     </ul>
                 </div>
                 <div className="navbar-end">
-                    <a className="btn">Login</a>
+                    <a className="btn">Logout</a>
                 </div>
             </div>
         </div>
